71ad2b3a6ec6d4e0e5679ffb0127e464263a9e77
Christian <chrherculano@gmail.com>
2023-04-24 12:56:39 -0300

    12112

--- script.js
+++ script.js
@@ -31,12 +31,20 @@ function displayQuote() {
 
 	quoteDisplay.innerHTML = letterSpans.join(""); // exibir a frase com cada letra em um intervalo
 	answerInput.value = ""; // limpar o input do usuario
+	
 }
 
 // verificação do texto
 function checkAnswer() {
     const answer = answerInput.value.toLowerCase();
     const quote = quotes[currentQuote].toLowerCase();
+
+    if (answer === quote) {
+       
+        quoteDisplay.querySelectorAll("span").forEach(span => span.style.color = "blue");
+        return true;
+    }
+
     const letters = quote.split("");
     let allCorrect = true;
 
@@ -44,40 +52,50 @@ function checkAnswer() {
         const letterSpan = document.getElementById(`letter-${i}`);
 
         if (answer[i] === letters[i]) {
-            letterSpan.style.color = "blue";
+            letterSpan.style.color = "blue";	 // deixa as letras azuis caso resposta seja = letras da frase
         } else if (answer[i]) {
-            letterSpan.style.color = "red";
+            letterSpan.style.color = "red";   //deixa vermelho caso não seja
             allCorrect = false;
         } else {
-            letterSpan.style.color = "black";
-            
+            letterSpan.style.color = "black";  // preto se receber nenhum valor do usuario
+			allCorrect = false;
+			
         }
-    } 
+    }
+	
 
-    return allCorrect; // retorna o valor 
+    return allCorrect;
 }
+
 
 
 // Evento do enter verifica se esta correto ou incorreto e alerta o usuario
 answerInput.addEventListener("keydown", function(event) {
 	if (event.key === "Enter" && !event.shiftKey) {
-	  event.preventDefault();
-	  if (checkAnswer()) {
-		alert("Nice! Você acertou!");
-		displayQuote();
-		var x = document.getElementById("Coin collect sound effect free to use.mp3");
-		x.play();
-	  } else {
-		alert("Você errou, tente novamente!");
-		answerInput.value = "";
-		checkAnswer(); 
-	  }
+		event.preventDefault(); // impede comando default do enter para quebrar linha
+		if (answerInput.value.trim().length > 0) { // garante que o input do usuario não seja
+			if (checkAnswer()) {
+				alert("Nice! Você acertou!");
+				displayQuote();
+				var x = document.getElementById("Coin collect sound effect free to use.mp3");
+				x.play();
+			} else {
+				alert("Você errou, tente novamente!");
+				answerInput.value = "";
+				quoteDisplay.querySelectorAll("span").forEach(span => span.style.color = "black");
+				displayCurrentQuote();
+			}
+		}
 	}
-  });
-  
+});
   
 // Chama função checkAnswer toda vez que o usuario digita ou excluir o texto no campo de resposta
-answerInput.addEventListener("input", checkAnswer); 
+answerInput.addEventListener("input", function() {
+    checkAnswer();
+    if (answerInput.value.length > quoteDisplay.children.length) {
+        answerInput.value = answerInput.value.slice(0, quoteDisplay.children.length);
+    }
+}); 
 
 // mostra a primeira frase ao carregar o site
 displayQuote();
