81446a41f6015c57dadfd7b86a3dabeeddaf245c
Christian <chrherculano@gmail.com>
2023-04-24 11:02:48 -0300

    salve

--- script.js
+++ script.js
@@ -9,19 +9,22 @@ const quotes = [
 	"Você pode observar muito apenas observando",
 	"Você pode observar muito apenas observando",
 	"Acredite que você pode e você já está no meio do caminho",
-	"Dança Gatinho dança"	
+	"Dança Gatinho dança",
+	"La ele",
+	"Desistir é para os fracos! Faça que nem eu, nem tente"
+
 ];
 
 let currentQuote = 0; 
 const answerInput = document.getElementById("answer");
 const quoteDisplay = document.getElementById("quote");
-
-
+const audio = document.getElementById("myaudio");
+audio.volume = 0.1;
 
 // função para mostrar uma frase aleatoria
 function displayQuote() {
 	currentQuote = Math.floor(Math.random() * quotes.length); // escolhe a frase pelo indice 
-	const quote = quotes[currentQuote];	// Variavel quote
+	const quote = quotes[currentQuote];	
 
 	// divida a citação em letras individuais e envolva cada uma em um intervalo com um ID exclusivo
 	const letterSpans = quote.split("").map((letter, i) => `<span id="letter-${i}">${letter}</span>`);
@@ -56,13 +59,15 @@ function checkAnswer() {
 	  }
 	}
   }
-  // Adiciona eventos para quando usuario aperta tecla Enter
+  
 answerInput.addEventListener("keydown", function(event) {
 	if (event.key === "Enter" && !event.shiftKey) {
 		event.preventDefault(); // impede comando default do enter para quebrar linha
 		checkAnswer(); // invoca a função de verifica se a frase está correto
 		alert("Nice Você acertou!"); // Alerta 
 		displayQuote();// exibe proxima frase
+		var x = document.getElementById("Coin collect sound effect free to use.mp3"); 
+			x.play();
 	}
 });
 
@@ -71,10 +76,9 @@ answerInput.addEventListener("keydown", function(event) {
 	if (event.shiftKey === "Enter") {
 		answerInput.value += "\n"; // atribuindo valor para key de quebrar a linha
 	}
-});	
+});
 // Chama função checkAnswer toda vez que o usuario digita ou excluir o texto no campo de resposta
 answerInput.addEventListener("input", checkAnswer); 
-
 
 // mostra a primeira frase ao carregar o site
 displayQuote();
